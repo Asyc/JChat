0588e53c72a66d15d41f3d88af69039df3fa1821
Asyc <aliheravi21@gmail.com>
2020-08-30 02:39:39 -0500

    Handshake Packets (Client)

--- Client/src/main/java/me/asyc/jchat/client/network/packet/impl/inbound/ServerHandshakePacket.java
+++ Client/src/main/java/me/asyc/jchat/client/network/packet/impl/inbound/ServerHandshakePacket.java
@@ -1,14 +1,24 @@
 package me.asyc.jchat.client.network.packet.impl.inbound;
 
 import io.netty.channel.Channel;
+import io.netty.util.AttributeKey;
+import me.asyc.jchat.client.JChatClient;
+import me.asyc.jchat.client.encryption.ClientCryptoManager;
 import me.asyc.jchat.client.network.packet.impl.outbound.ClientHandshakePacket;
+import me.asyc.jchat.network.encryption.CryptoKey;
+import me.asyc.jchat.network.encryption.CryptoManager;
 import me.asyc.jchat.network.packet.InboundPacket;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
 public final class ServerHandshakePacket implements InboundPacket {
+
+    private static final AttributeKey<PublicKey> RSA_KEY_ATTRIBUTE = AttributeKey.valueOf("jchat:rsa_key");
+    private static final AttributeKey<CryptoKey> KEY_ATTRIBUTE = AttributeKey.valueOf("jchat:key");
 
     private Channel channel;
     private byte[] key, validate;
@@ -18,15 +28,19 @@ public final class ServerHandshakePacket implements InboundPacket {
         this.channel = channel;
 
         DataInputStream in = new DataInputStream(new ByteArrayInputStream(buffer));
-        this.key = new byte[in.readInt()];
+        this.key = new byte[128];
         in.readFully(this.key);
-        this.validate = new byte[in.readInt()];
+
+        this.validate = new byte[128];
         in.readFully(this.validate);
     }
 
     @Override
-    public void handle() {
-        this.channel.writeAndFlush(new ClientHandshakePacket(this.key, this.validate));
+    public void handle() throws Exception {
+        ClientCryptoManager cryptoManager = JChatClient.INSTANCE.getCryptoManager();
+        this.channel.attr(ServerHandshakePacket.RSA_KEY_ATTRIBUTE).set(cryptoManager.createKeyFromBufferRSA(this.key));
+
+        this.channel.writeAndFlush(new ClientHandshakePacket(cryptoManager.generateKey(), this.validate));
     }
 
     @Override
--- Client/src/main/java/me/asyc/jchat/client/network/packet/impl/outbound/ClientHandshakePacket.java
+++ Client/src/main/java/me/asyc/jchat/client/network/packet/impl/outbound/ClientHandshakePacket.java
@@ -3,29 +3,45 @@ package me.asyc.jchat.client.network.packet.impl.outbound;
 import io.netty.channel.Channel;
 import io.netty.util.AttributeKey;
 import me.asyc.jchat.client.JChatClient;
+import me.asyc.jchat.client.encryption.ClientCryptoManager;
+import me.asyc.jchat.network.encryption.CryptoKey;
 import me.asyc.jchat.network.packet.OutboundPacket;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.security.PublicKey;
 
 public class ClientHandshakePacket implements OutboundPacket {
 
     private static final AttributeKey<PublicKey> RSA_KEY_ATTRIBUTE = AttributeKey.valueOf("jchat:rsa_key");
+    private static final AttributeKey<CryptoKey> KEY_ATTRIBUTE = AttributeKey.valueOf("jchat:key");
 
-    private final byte[] secret, validate;
+    private Channel channel;
 
-    public ClientHandshakePacket(byte[] secret, byte[] validate) {
+    private final CryptoKey secret;
+    private final byte[] validate;
+
+    public ClientHandshakePacket(CryptoKey secret, byte[] validate) {
         this.secret = secret;
         this.validate = validate;
     }
 
     @Override
     public void write(Channel channel, OutputStream stream) throws Exception {
+        this.channel = channel;
+
+        ClientCryptoManager cryptoManager = JChatClient.INSTANCE.getCryptoManager();
+
         DataOutputStream out = new DataOutputStream(stream);
-        out.write(JChatClient.INSTANCE.getCryptoManager().encryptRSA(channel.attr(ClientHandshakePacket.RSA_KEY_ATTRIBUTE).get(), this.secret));
-        out.write(JChatClient.INSTANCE.getCryptoManager().encryptRSA(channel.attr(ClientHandshakePacket.RSA_KEY_ATTRIBUTE).get(), this.validate));
+
+        PublicKey key = channel.attr(ClientHandshakePacket.RSA_KEY_ATTRIBUTE).get();
+        out.write(cryptoManager.encryptRSA(key, this.secret.getKey().getEncoded()));
+        out.write(cryptoManager.encryptRSA(key, cryptoManager.encrypt(this.secret, this.validate)));
+    }
+
+    @Override
+    public void postWrite() {
+        this.channel.attr(ClientHandshakePacket.KEY_ATTRIBUTE).set(this.secret);
     }
 
     @Override
